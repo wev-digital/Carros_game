13346122092c97e47019543b6061f58ada88b4a9
wev_digital <Wevertonmartinsdigital@gmail.com>
2024-12-07 13:23:00 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function menu_mobile() {
     var menu_oculto = document.querySelector('.nav_listas');
 
-    if (menu_oculto.style.display === "none") {
-        menu_oculto.style.display = "block";
-    } else {
+    if (menu_oculto.style.display === "block") {
         menu_oculto.style.display = "none";
+    } else { 
+        menu_oculto.style.display = "block"; 
     }
 }
